f8d523a83a78db7be58edc7c36f843c0d998c5b7
Tyler Miller <tyler.rw.miller@gmail.com>
2019-01-19 12:36:59 -0500

    Finish CoffeeMaker Project Step 7: Test

--- CoffeeMaker/src/test/CoffeeMakerTest.java
+++ CoffeeMaker/src/test/CoffeeMakerTest.java
@@ -1,21 +1,87 @@
 package test;
 
+import exceptions.*;
+import model.CoffeeMaker;
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.fail;
+
 
 public class CoffeeMakerTest {
+    private CoffeeMaker cm;
 
     @Before
     public void setUp(){
-        // TODO: instantiate your test objects here
+        CoffeeMaker cm = new CoffeeMaker();
     }
-
+    @Test (expected = NotEnoughBeansException.class)
+    public void TestBrewNotEnoughBeans() throws NotEnoughBeansException, TooManyBeansException, WaterException {
+        cm.brew(2.3, 14.76);
+    }
+    @Test (expected = TooManyBeansException.class)
+    public void TestBrewFailTooManyBeans() throws NotEnoughBeansException, TooManyBeansException, WaterException {
+        cm.brew(2.7, 14.76);
+    }
+    @Test (expected = WaterException.class)
+    public void TestBrewFailNotEnoughWater() throws NotEnoughBeansException, TooManyBeansException, WaterException {
+        cm.brew(2.6, 2);
+    }
+    @Test (expected = WaterException.class)
+    public void TestBrewFailBothParamsWrongThrowWaterFirst() throws NotEnoughBeansException, TooManyBeansException, WaterException {
+        cm.brew(2.7, 2);
+    }
     @Test
-    public void testTemplate(){
-        // TODO: write some more tests
-        // This is a template for unit tests
+    public void TestBrewPass(){
+        try{
+            brewCoffee();
+            assertEquals(20, cm.getCupsRemaining());
+            assertEquals(0, cm.getTimeSinceLastBrew());
+        }
+        catch (Exception e){
+            fail();
+        }
+    }
+    @Test (expected = NoCupsRemainingException.class)
+    public void TestPourCoffeeFailNoCups() throws NoCupsRemainingException, StaleCoffeeException, NotEnoughBeansException, TooManyBeansException, WaterException {
+        brewCoffee();
+        pour20COffees();
+        cm.pourCoffee();
+    }
+    @Test (expected = StaleCoffeeException.class)
+    public void TestPourCoffeeFailStaleCoffee() throws NoCupsRemainingException, StaleCoffeeException, NotEnoughBeansException, TooManyBeansException, WaterException {
+        brewCoffee();
+        cm.setTimeSinceLastBrew(60);
+        cm.pourCoffee();
+    }
+    @Test (expected = NoCupsRemainingException.class)
+    public void TestPourCoffeeFailBothParamsWrongNoCupsFirst() throws NoCupsRemainingException, StaleCoffeeException, NotEnoughBeansException, TooManyBeansException, WaterException {
+        brewCoffee();
+        pour20COffees();
+        cm.setTimeSinceLastBrew(60);
+        cm.pourCoffee();
+    }
+    @Test
+    public void TestPourCoffeePass() throws NotEnoughBeansException, TooManyBeansException, WaterException {
+        brewCoffee();
+        try{
+            cm.pourCoffee();
+            assertEquals(19, cm.getCupsRemaining());
+        }
+        catch (Exception e){
+            fail();
+        }
+
     }
 
+    public void brewCoffee() throws NotEnoughBeansException, TooManyBeansException, WaterException {
+        cm.brew(2.7, 17);
+    }
 
+    public void pour20COffees() throws NoCupsRemainingException, StaleCoffeeException {
+        for (int i = 20; i >= 0; i--){
+            cm.pourCoffee();
+        }
+    }
 }
