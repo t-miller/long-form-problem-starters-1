c433c9fc562ba20546443b4e573cf8df4d6c655f
Tyler Miller <tyler.rw.miller@gmail.com>
2019-01-21 14:42:53 -0500

    Finish Busy'sDiner project Step 10: Refactoring

---
+++ Busy'sDiner/Busy'sDiner3/src/model/FOHEmployee.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class FOHEmployee {
+    private Dish dish;
+
+    public FOHEmployee(Dish dish) {
+        this.dish = dish;
+    }
+
+    public abstract String getPrefix();
+
+    //EFFECTS: prints out a description of the dish on the menu
+    public void describeDish() {
+        System.out.println(dish.getDescription());
+    }
+
+    //EFFECTS: prints out a greeting
+    public void greet() {
+        System.out.println("\"Hello and welcome to Busy's, the home of the trendy turkey club sandwich.\"");
+    }
+
+    //MODIFIES: this, order
+    //EFFECTS: logs order as served and brings to table
+    public void deliverFood(Order order) {
+        order.setIsServed();
+        System.out.print(getPrefix() + "Delivered food: ");
+        order.print();
+    }
+}
